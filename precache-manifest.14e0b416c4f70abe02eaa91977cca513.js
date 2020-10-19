d332452f37049cba74b7ddecc563d4b43ff4781c
Somsubhra Das <das.somsubhra1@gmail.com>
2020-10-20 00:34:16 +0530

    Updates

---
+++ precache-manifest.14e0b416c4f70abe02eaa91977cca513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3d441fb9275d080a51e69a36b6bb7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a877502fa264f2e812ed",
+    "url": "/static/css/main.06bb305b.chunk.css"
+  },
+  {
+    "revision": "3cf4997e45da9112420d",
+    "url": "/static/js/2.309e7ac6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.309e7ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a877502fa264f2e812ed",
+    "url": "/static/js/main.f591f5e0.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
